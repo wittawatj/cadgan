b54ba8feb171bd5d851f61ecb5bf5aa46f157d85
wgondal <waleed.gondal@tuebingen.mpg.de>
2018-12-04 17:53:13 +0100

    scripts for training extractors predicting the presence of different concepts in the scene, based on coco-stuff dataset

---
+++ prob_models/coco_dataset.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""Loads image datasets from folders of images."""
+
+from PIL import Image
+from os import listdir
+from os.path import join
+import torch.utils.data as data
+import numpy as np
+import random
+from scipy.misc import imread, imresize
+import torch
+from pycocotools.coco import COCO
+import os
+from torchvision.transforms import Compose, CenterCrop, ToTensor
+
+def is_image_file(filename, extensions):
+    return any(filename.lower().endswith(ext) for ext in extensions)
+
+def load_img(filepath):
+    return Image.open(filepath).convert('RGB')
+
+def _scale_and_crop(img, seg_mask, cropSize, is_train):
+    h, w = np.size(img,0), np.size(img,1)
+    if is_train:
+        # random scale
+        scale = random.random() + 0.2     # 0.5-1.5
+        scale = max(scale, 1. * cropSize / (min(h, w) - 1))
+    else:
+        # scale to crop size
+        scale = 1. * cropSize / (min(h, w) - 1)
+
+    img = imresize(img, scale, interp='bicubic')
+    try:
+        seg = imresize(seg_mask, scale, interp='bilinear')
+    except:
+        seg = np.full((h,w), 182) #An issue with one image, thus returning dummy mask.
+    h_s, w_s = img.shape[0], img.shape[1]
+    x1 = random.randint(0, w_s - cropSize)
+    y1 = random.randint(0, h_s - cropSize)
+
+    img_crop = img[y1: y1 + cropSize, x1: x1 + cropSize, :]
+    seg_crop = seg[y1: y1 + cropSize, x1: x1 + cropSize]
+    return img_crop, seg_crop
+
+
+def return_segMask(anns, coco):
+    '''Return 6 segmentaion masks sorted on the basis of their area. The 7th mask belongs to others class'''
+    mask = [(coco.annToMask(m))*( m['category_id']-1) for m in anns] #coco.annToMask(m)-> mask of 1s and 0s
+    return sum(mask)
+
+def target_transform(crop_size):
+    return Compose([CenterCrop(crop_size), ToTensor()])
+
+def return_labels(mask, num_of_classes):
+    unique_labels = torch.FloatTensor(np.unique(mask))
+    labels = torch.zeros(num_of_classes)
+    labels[unique_labels.long()] = 1
+    return labels
+
+
+class Dataset_CocoSegmented(data.Dataset):
+    def __init__(self, path, cropsize, annotationFile,
+                 image_extensions=(".png", ".jpg", ".jpeg"), is_train = False, bicubic = False):
+        super(Dataset_CocoSegmented, self).__init__()
+        self.is_train = is_train
+        self.cropsize = cropsize
+        self.bicubic = bicubic
+        self.root = path
+        self.annFile = annotationFile #'/home/wgondal/coco_stuff/stuff_train2017.json'
+        self.coco = COCO(self.annFile)
+        self.imgIds = self.coco.getImgIds()
+
+    def __getitem__(self, index):
+        img_id = self.imgIds[index]
+        annIds = self.coco.getAnnIds(imgIds=img_id)
+        anns = self.coco.loadAnns(annIds)
+        seg_mask = return_segMask(anns, self.coco)
+        path = self.coco.loadImgs(img_id)[0]['file_name']
+        img = load_img(os.path.join(self.root, path))
+        target, scaled_and_cropped_seg_mask = _scale_and_crop(img, seg_mask, self.cropsize, is_train = True)
+
+        labels = return_labels(scaled_and_cropped_seg_mask, 183)
+        target = target.astype(np.float32) / 255.
+        target = target.transpose((2, 0, 1))
+        target = torch.from_numpy(target)
+        return target, labels
+
+    def __len__(self):
+        return len(self.imgIds)
---
+++ prob_models/train_extractor.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+""" Feature Extraction, using multiplabel classification using MS-COCO stuff dataset with 150 natural classes.
+"""
+import datetime
+import argparse
+import os
+from math import log10
+import torch
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from extractors import ResNet18_365
+import torchvision
+from coco_dataset import Dataset_CocoSegmented
+import torch.nn as nn
+from tensorboardX import SummaryWriter
+
+def to_variable(x):
+    """Convert tensor to variable."""
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+
+def denorm_clamp(x):
+    return x.clamp(0, 1)
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MultiLabel Classification')
+parser.add_argument('--feat_size', type=int, default=64, help="number of feature channels. Default=64")
+parser.add_argument('--cropsize', type=int, default=256, help="image cropsize. Default=128")
+parser.add_argument('--batch_size', type=int, default= 32, help='training batch size. Default=16')
+parser.add_argument('--epochs', type=int, default=150, help='number of epochs to train for. Default=100')
+parser.add_argument('--lr', type=float, default=0.0003, help='Learning Rate. Default=0.001')
+parser.add_argument('--segmented_input', type=int, default=1, help='segment input images with pretrained semantic segmentation model before feeding into G. Default=0')
+parser.add_argument('--loss_mse', type=float, default=1, help='weight of MSE loss. Default=1')
+parser.add_argument('--cuda', type=int, default=1, help='Try to use cuda? Default=1')
+parser.add_argument('--resume',default = None, help='Provide an epoch to resume training from. Default=None')
+parser.add_argument('--log_dir', default = './extractor_tb_log/', help='Log dir for tensorboardX output logs')
+parser.add_argument('--threads', type=int, default=6, help='number of threads for data loader to use. Default=4')
+parser.add_argument('--seed', type=int, default=1, help='random seed to use. Default=1')
+parser.add_argument('--annFile', type=str, default='/home/wgondal/coco_stuff/stuff_train2017.json', help='Provide MS-COCO Stuff Segmented Annotation File. Default=0')
+parser.add_argument('--path_data', type=str, default='/agbs/cpr/train2017/', help='path of train images. Default=ADE20K')
+opt = parser.parse_args()
+
+cuda = False
+if opt.cuda:
+    if torch.cuda.is_available():
+        cuda = True
+        torch.cuda.manual_seed(opt.seed)
+    else:
+        print('===> Warning: failed to load CUDA, running on CPU!')
+
+# ==================  Creating tensorboardX log ============================
+now = datetime.datetime.now()
+training_specs = 'lr_{}_bs_{}__{}_{}-{}_{}{}'.format(opt.lr, opt.batch_size,
+                now.month, now.day,now.hour, now.minute, now.second)
+log_folder_path = os.path.join(opt.log_dir, training_specs)
+writer = SummaryWriter(log_dir=log_folder_path)
+
+print('===> Loading datasets')
+train_set = Dataset_CocoSegmented(path=opt.path_data, cropsize= opt.cropsize,
+            annotationFile = opt.annFile)
+
+print('===> Found %d training images.' % len(train_set))
+training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads,
+                                  batch_size=opt.batch_size, shuffle=True)
+
+print('===> Building model')
+if opt.resume:
+    print('Using Pre-trained Generator model')
+    G = torch.load(resume)
+    G.train()
+else:
+    G = ResNet18_365(num_classes = 183)
+    if cuda:
+        G = G.cuda()
+
+'''ct = 0
+for child in G.children():
+    ct += 1
+    if ct != 3:
+        for param in child.parameters():
+            param.requires_grad = False'''
+
+
+print('===> Building optimizer')
+optimizer = optim.Adam(G.parameters(), lr=opt.lr)
+#optimizer = optim.Adam(filter(lambda p: p.requires_grad, G.parameters()), lr=opt.lr)
+
+experiment = 'multilabel'
+saves_path = './Extractors/'+experiment+'/saves'
+
+if not os.path.exists(saves_path):
+    os.makedirs(saves_path)
+
+l1_loss = nn.L1Loss().cuda()
+l2_loss = nn.MSELoss().cuda()
+
+#crit = nn.NLLLoss(ignore_index=-1)
+crit = nn.BCEWithLogitsLoss() # sigmoid included
+
+print('===> Initializing training')
+def train(epoch):
+    steps = 0
+
+    for iteration, batch in enumerate(training_data_loader, 1):
+        data = to_variable(batch[0])
+        label = to_variable(batch[1])#.long()
+        pred = G(data)
+        loss = crit(pred, label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        writer.add_scalar('loss', loss.item(), steps)
+        steps +=1
+        if iteration%50 == 0:
+            print('Epoch [%d], Step[%d/%d], overall_loss: %.8f'
+              %(epoch, iteration, len(training_data_loader), loss.data[0]))
+
+
+def save_checkpoint(epoch):
+    G_out_path ='%s/epoch_%s.pth'%(saves_path,str(epoch))
+    if not os.path.exists(os.path.dirname(G_out_path)):
+        os.makedirs(os.path.dirname(G_out_path))
+    torch.save(G, G_out_path)
+    print("Checkpoint saved to {}".format(G_out_path))
+
+for epoch in range(1, opt.epochs + 1):
+    train(epoch)
+    if epoch % 3 == 0:
+        save_checkpoint(epoch)
+
+    # Learning rate decay
+    if (epoch+1)==80 or (epoch+1)==110:
+        for g in optimizer.param_groups:
+            g['lr'] = g['lr'] * 0.1
+            writer.add_scalar('learning_rate', g['lr'], steps)
