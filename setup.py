ae8f7c5db227650e8e66ae5ebcf80de30d498b37
Patsorn <psangkloy3@gatech.edu>
2019-06-29 12:30:32 -0400

    add ganstab dependency

--- setup.py
+++ setup.py
@@ -62,5 +62,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["numpy", "autograd", "scipy", "matplotlib"],
+    install_requires=["numpy", "autograd", "scipy", "matplotlib","ganstab @ git+https://github.com/wittawatj/GAN_stability.git#egg=ganstab-0.1.0"],
 )
