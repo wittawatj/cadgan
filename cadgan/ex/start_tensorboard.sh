fe8d0c8f0cf053bc6d8ee35f5b9dd1d474627e81
janesjanes <patsorn.sangkloy@gmail.com>
2019-11-24 08:00:13 -0500

    remove batch files

--- cadgan/ex/start_tensorboard.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# One input argument: Tensorboard log folder
-python3 -m tensorboard.main --logdir $1
--- cadgan/ex/test_n1_49_imgstep_1.sh
+++
@@ -1,1 +0,0 @@
-srun -p short --gres=gpu:1 --exclude bender bash /nethome/psangkloy3/dockerfile/run_docker_kbrgan_script.sh "/notebooks/psangkloy3/kbrgan_temp/kbrgan/ex/test_n1_49_imgstep_1/" "e7cad0368cff.sh"& 
